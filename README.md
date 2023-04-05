86b4c15f4541b01f7297862c2fdec3e5d8371312
Mandarinfish <66844643+Mandarinfish1@users.noreply.github.com>
2023-04-04 20:29:05 -0400

    updating script.js/style.js/readme

---
+++ README.md
@@ -0,0 +1,75 @@
+# Title
+
+# Weather Dashboard
+
+## License
+
+[![license](https://img.shields.io/badge/License-MIT-green)](https://opensource.org/licenses/MIT)
+
+## Description
+
+- To create a Weather Dashboard that retrieves the 5 Day Weather Forecast for cities in the U.S. using HTML, CSS, Javascript and OpenWeatherMap.org.
+
+## Table of Contents
+
+- [Title](#title)
+- [License](#license)
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Demo](#demo)
+- [Contributing](#contributing)
+- [Credits](#credits)
+- [Reference](#reference)
+- [Test Instructions](#test-instructions)
+- [Completed Project Link](#completed-project-link)
+- [Questions](#questions)
+
+## Installation
+
+- The Weather Dashboard does not need any installation.
+
+## Usage
+
+- To use the Weather Dashboard. Run the deployed repository in your browser and it will give you a Weather app. Enter a city to see the current weather and 5 day forecast.
+
+## Demo
+
+- ![Screenshot-1](https://user-images.githubusercontent.com/66844643/229950668-c35549f1-e37d-468a-806a-4ca8bd791ede.png)
+
+- ![Screenshot-2](https://user-images.githubusercontent.com/66844643/229950794-81bc7d80-7665-4079-b8b6-99f713934805.png)
+
+- ![Screenshot-3](https://user-images.githubusercontent.com/66844643/229950837-a6074fa5-12b3-49d5-b5c0-6f7d4e376a7a.png)
+
+
+## Contributing
+
+- No contributions.
+
+## Credits
+
+- Blue Sky Photo by: https://www.pexels.com/@didsss/
+
+## Reference
+
+- University of Richmond Bootcamp
+
+
+## Test Instructions
+
+- In order to test the Weather Dashboard, run the app from the repository in your browswer. If any errors occur, please check the console. It will tell you what errors are occuring.
+
+# Deployed Repository Link
+
+- 
+
+# Weather Dashboard Repostory Link
+
+- https://github.com/Mandarinfish1/Weather-Dashboard
+
+# Questions
+
+- If you have questions, I can be reached by:
+- Email @ ruizpeggys@yahoo.com
+- Github Username: Mandarinfish1
+- Github Profile Link: https://github.com/Mandarinfish1
--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+//API Key to access weather data from openweathermap.org
 const API_KEY = '09d7eef7116fab393a87b86755626c97';
+
+//HTML elements from the HTML file to be used as variables in script.js file
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const currentWeather = document.getElementById('current-weather');
 const forecast = document.getElementById('forecast');
 
+//Using an event listener for the search form to listen for a submit event.
+//If a city is submitted, then the fetchWeather(city) function is executed.
 searchForm.addEventListener('submit', event => {
     event.preventDefault();
     const city = searchInput.value;
@@ -12,17 +17,19 @@ searchForm.addEventListener('submit', event => {
     }
 });
 
+//Fetching weather data for a submitted city.
+//Using async/await to wait for fetch request to complete before continuing. 
 async function fetchWeather(city) {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`); 
     const data = await response.json();
-    if (data.cod === 200) {
+    if (data.cod === 200) { //if API request successful (code 200) then executes displayCurrentWeather function. 
         displayCurrentWeather(data);
         fetchForecast(city);
     } else {
         alert('City not found. Please try again.');
     }
 }
-
+//Using template literals to update values for current weather temp, humidity, wind speed for city submitted.
 function displayCurrentWeather(data) {
     const weatherIconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
     const html = `
@@ -33,14 +40,16 @@ function displayCurrentWeather(data) {
         <p>Wind Speed: ${data.wind.speed} m/s</p>
     `;
     currentWeather.innerHTML = html;
+    //Unhiding the currentWeather to make it visible.
+    currentWeather.classList.remove('hidden');
 }
-
+//Fetch the 5-day forecast data for the submitted city.
 async function fetchForecast(city) {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}&units=metric`);
     const data = await response.json();
     displayForecast(data.list);
 }
-
+//Display the 5-day forecast data for the submitted city using template literals.
 function displayForecast(forecastData) {
     const dailyData = forecastData.filter((item, index) => index % 8 === 0);
     let html = '';
@@ -57,4 +66,11 @@ function displayForecast(forecastData) {
         `;
     });
     forecast.innerHTML = html;
-}
+    //Unhiding the forecast heading once the forecast data has been fetched and displayed.
+    document.getElementById('forecast-heading').classList.remove('hidden');
+    }
+
+
+
+
+
